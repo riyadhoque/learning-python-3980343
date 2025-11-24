d703053bdb0f83cb380bb4275c1f65d3be0bb527
riyadhoque <146047849+riyadhoque@users.noreply.github.com>
2025-11-23 19:08:01 -0500

    Update variables_start.py

--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -10,7 +10,8 @@ mystr = "This is a string"
 mybool = True
 
 # We can display the content of a variable using the print() function
-
+print(myint)
+print(mystring)
 
 # Operators are used to perform operations on variables
 
